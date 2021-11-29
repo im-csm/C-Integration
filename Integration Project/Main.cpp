c188807c45f30c55846cf41163b696e3409c2b70
Chance Mullen <csmullen8509@eagle.fgcu.edu>
2021-11-28 23:14:05 -0500

    Included functionality for SFML to create a simple game.

--- Integration Project/Main.cpp
+++ Integration Project/Main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <string>
 #include "Automobile.h"
-
-using namespace std;
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
+	//sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Integration Project");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear();
+			window.draw(shape);
+			window.display();
+		}
+	}
+
+	/*
 	Automobile zoomies;
 	zoomies.setMake("Honda Accord");
 	zoomies.setWheels(4);
@@ -16,5 +41,8 @@ int main()
 		cout << "Time since launch: " << ZeroTo60 << " seconds." << "\n";
 		cout << "Current Speed: " << zoomies.getSpeed() << "\n";
 	}
+	*/
+
 	return 0;
 }
+
