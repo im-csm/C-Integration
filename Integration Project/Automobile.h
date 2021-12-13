316ff6aafa0d0049f14625cee60341b8397fc283
Chance Mullen <csmullen8509@eagle.fgcu.edu>
2021-12-13 02:25:26 -0500

    Some Updates

--- Integration Project/Automobile.h
+++ Integration Project/Automobile.h
@@ -6,17 +6,17 @@
 
 class Automobile
 {
-private:
-	int num_wheels;
-	int speed{ 0 };
-	std::string make;
-	int model_year;
 public:
 	void setMake(std::string company_name);
 	void setWheels(int wheel_count);
 	void Accelerate();
 	void brake();
 	int getSpeed();
+private:
+	int num_wheels;
+	int speed{ 0 };
+	std::string make;
+	int model_year;
 };
 
 #endif Automobile_H_
--- Integration Project/Main.cpp
+++ Integration Project/Main.cpp
@@ -6,29 +6,95 @@
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
 
+float calculateTheta(float x_diff, float y_diff) 
+{
+	int theta;
+	return theta;
+}
+
 int main()
 {
-	//sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Integration Project");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	// Create Window
+	int window_h = 800, window_w = 600;
+	sf::RenderWindow window(sf::VideoMode(window_h, window_w), "Integration Project");
+	window.setMouseCursorVisible(true);
+	window.setFramerateLimit(60);
 
+
+	// The hero of the adventure. A cyan circle standing in the middle of the apocalypse.
+	// Defend your glory or perish.
+	sf::CircleShape hero(25.f);
+	hero.setFillColor(sf::Color::Cyan);
+	hero.setOrigin(hero.getRadius(), hero.getRadius());
+	hero.setPosition(sf::Vector2f(window_h / 2.f, window_w / 2.f));
+	auto heroBounds = hero.getTextureRect();
+
+	
+	// Gun
+
+	sf::RectangleShape gun(sf::Vector2f(7.f, 25.f));
+	gun.setFillColor(sf::Color::White);
+	gun.setOrigin(gun.getLocalBounds().width / 2, gun.getLocalBounds().height + 6);
+	gun.setPosition(hero.getPosition().x, hero.getPosition().y);
+	gun.setRotation(90);
+	
+	//Gun Tip ** COMING BACK TO THIS **
+	/*
+	sf::RectangleShape tip(sf::Vector2f(5.f, 2.f));
+	tip.setFillColor(sf::Color::Color(253, 177, 24));
+	tip.setRotation(gun.getRotation());
+	tip.setOrigin(2.5, 1.f);
+	tip.setPosition(gun.getPosition().x, gun.getPosition().y - 31 );
+	//tip.setPosition(window_h / 2.f, window_w / 2.f);
+	*/
+
+	// Main loop
 	while (window.isOpen())
 	{
-		while (window.isOpen())
-		{
-			sf::Event event;
-			while (window.pollEvent(event))
-			{
-				if (event.type == sf::Event::Closed)
-					window.close();
-			}
+		
+		// Variables
+		auto mouseX = sf::Mouse::getPosition(window).x;
+		auto mouseY = sf::Mouse::getPosition(window).y;
+		//float *heroX = &hero_x;
+		//float *heroY = &hero_y;
+		//float heroEdgeX = *heroX + hero.getRadius();
+		//float heroEdgeY = *heroY + hero.getRadius();
+		//hero.setPosition( sf::Vector2f(window_h / 2.f, window_w / 2.f) );
+		//hero.setPosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
 
-			window.clear();
-			window.draw(shape);
-			window.display();
+		// Event obect
+		sf::Event ev;
+		
+		// Loop checks for an event
+		while (window.pollEvent(ev))
+		{
+			// Top right X clicked to close the window
+			if (ev.type == sf::Event::Closed)
+				window.close();
+			if (ev.type == sf::Event::MouseButtonPressed)
+			{
+				std::cout << "Mouse X pos: " << mouseX << "Mouse Y pos: " << mouseY << "\n";
+				std::cout << "x: " << gun.getPosition().x << " y: " << gun.getPosition().y << "\n";
+				//std::cout << "Hero x: " << tip.getPosition().x << " Hero y: " << tip.getPosition().y << "\n";
+			}
 		}
+		
+		// Clears current frame
+		window.clear();
+
+		// Draw shapes to the screen for new frame
+		window.draw(hero);
+		window.draw(gun);
+		//window.draw(tip);
+
+
+		// Display new window frame
+		window.display();
 	}
+
+
+	return 0;
+}
 
 	/*
 	Automobile zoomies;
@@ -42,7 +108,3 @@ int main()
 		cout << "Current Speed: " << zoomies.getSpeed() << "\n";
 	}
 	*/
-
-	return 0;
-}
-
