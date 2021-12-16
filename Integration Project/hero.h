052a1003a2e27fff835eb60955f7b12beefbf113
im-csm <71950678+im-csm@users.noreply.github.com>
2021-12-15 19:09:28 -0500

    Update hero.h
    Comments for learning objectives.

--- Integration Project/hero.h
+++ Integration Project/hero.h
@@ -5,11 +5,24 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
+/** @hero hero.h
+ *  @brief Hero class .h file
+ *  LO1. Design and implement a class
+ *  
+ *  LO4. Procedural/functional programming is using a bunch of functions to
+ *       write/control a program. This differs from OOP in that OOP has objects,
+ *       and objects have attributes and methods. In short however, OOP is bottom-up
+ *	 rather than the top-down approach that procedural programming takes.
+ *
+ *  @author Chance Mullen
+ *  @bug No known bugs.
+ */
+
 class hero
 {
 private:
-	/* Hero attributes */ 
-
+	/* Hero attributes */
+	
 	float radius{25.f};
 	float windowX {}, windowY {};
 	float gunRotation;
@@ -26,7 +39,7 @@ public:
 	virtual void draw(GameWindow* target); // Draw Hero to screen
 	void initHero(); // Initialize hero attributes (Hero, Hero hitbox, and Hero gun.)
 	void setColor(sf::Color newColor); // Change Hero color
-
+	sf::Vector2f rotateGun(GameWindow* window); // Calculate the rotation of the gun based upon where the mouse is on the screen
 };
 
 #endif
