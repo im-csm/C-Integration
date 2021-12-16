6f327c62c20b79fca5a843337e172a0bc8585d81
im-csm <71950678+im-csm@users.noreply.github.com>
2021-12-15 19:38:46 -0500

    Update GameWindow.h

--- Integration Project/GameWindow.h
+++ Integration Project/GameWindow.h
@@ -4,6 +4,18 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+/** @GameWindow GameWindow.h
+ *  @Creates the unique window for which the game is played in.
+ *
+ *  LO2. This GameWindow class utilizes subclassing to inherit from sf::RenderWindow.
+ *  This allows me to use RenderWindow methods and return them for functions using a specific GameWindow Object
+ *    - LO2a. The visibility in this class is public. This allows me to still access public and private members of
+ 	RenderObject.
+ *
+ *  @author Your Name
+ *  @bug No known bugs.
+ */
+
 class GameWindow : public sf::RenderWindow
 {
 private:
