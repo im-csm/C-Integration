c49fbda093e85e4ec6066122c78d2dbef2a11a0e
im-csm <71950678+im-csm@users.noreply.github.com>
2021-12-15 19:44:39 -0500

    Update enemy.h

--- Integration Project/enemy.h
+++ Integration Project/enemy.h
@@ -8,16 +8,20 @@
 class enemy : public sf::Drawable
 {
 public:
-	enemy() : hitpointCount(3), radius(20) {}
-	enemy(sf::Color newColor, float radius, sf::Vector2f spawnPos): color(newColor) {}
+	// LO1b / LO1c
+	// enemy default sets the default hitpointCount, and radius.
+	enemy() : hitpointCount(3), radius(20.f) {}
+	// The overloaded constructor takes a new sf::Color, a float for the radius, and a new spawn position. 
+	enemy(sf::Color newColor, float radius, sf::Vector2f spawnPos): color(newColor) , radius(radius), spawnPosition(spawnPos) {}
 
 
 private:
-	int hitpointCount;
-	int score_multiplier;
-	float radius;
-	float size_multiplier;
-	bool isDead;
+	int hitpointCount {0} ;
+	int score_multiplier {0} ;
+	float radius {0.f};
+	float size_multiplier {1.f} ;
+	bool isDead {false} ;
+	sf::Vector2f spawnPosition {} ;
 	sf::Color color;
 };
 
